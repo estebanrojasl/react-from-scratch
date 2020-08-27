39677ea8093010d5784e1898833d0355d3933c85
esteban7590 <esteban7590@hotmail.com>
2020-08-27 11:19:00 -0500

    parenent/child component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import MyInfo from "./components/MyInfo.js";
 
-ReactDOM.render(<MyInfo />, document.getElementById("root"));
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
