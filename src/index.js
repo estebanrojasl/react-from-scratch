2e25dead0949b047da91819c9133a1d0bbf7e884
esteban7590 <esteban7590@hotmail.com>
2020-08-27 10:41:57 -0500

    components into separate files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-const MyInfo = () => {
-  return (
-    <div>
-      <h1>Esteban Rojas</h1>
-      <p>I am fc*ing awesome</p>
-      <ul>
-        <li>Canada</li>
-        <li>Iceland again</li>
-        <li>India</li>
-      </ul>
-    </div>
-  );
-};
+import MyInfo from "./components/MyInfo.js";
 
 ReactDOM.render(<MyInfo />, document.getElementById("root"));
