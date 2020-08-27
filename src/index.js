11ebb5b238d4fcee6c4fc22cd89f5be9d5451da1
esteban7590 <esteban7590@hotmail.com>
2020-08-27 14:54:11 -0500

    Inline styles

--- src/index.js
+++ src/index.js
@@ -3,21 +3,32 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 const App = () => {
-  const date = new Date();
-  const month = date.getMonth;
+  const date = new Date(2020, 5, 12, 10);
+  const month = date.getMonth();
+  console.log(month);
+  const styles = {};
 
   let quarter = null;
 
   if (month > 0 && month <= 3) {
     quarter = "first";
+    styles.color = "blue";
   } else if (month > 3 && month <= 6) {
     quarter = "second";
+    styles.color = "green";
   } else if (month > 6 && month <= 9) {
     quarter = "third";
+    styles.color = "yellow";
   } else {
     quarter = "forth";
+    styles.color = "gray";
   }
-  return <h1>Hey, we are in the {`${quarter} quarter`} of the year</h1>;
+
+  return (
+    <h1 style={styles}>
+      Hey, we are in the {`${quarter} quarter`} of the year
+    </h1>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
