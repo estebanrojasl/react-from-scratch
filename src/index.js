0bbe078189c800c60110eee6b804946fbdccd861
esteban7590 <esteban7590@hotmail.com>
2020-08-30 10:59:44 -0500

    class based components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,79 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-import App from "./components/App";
-
 import "./index.css";
+
+// #1
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <Greeting />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header username="Dickinson" />
+        <Greeting />
+      </div>
+    );
+  }
+}
+
+// // #2
+// function Header(props) {
+//   return (
+//     <header>
+//       <p>Welcome, {props.username}!</p>
+//     </header>
+//   );
+// }
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <p>Welcome, {this.props.username}!</p>
+      </header>
+    );
+  }
+}
+
+// #3
+// function Greeting() {
+//   const date = new Date();
+//   const hours = date.getHours();
+//   let timeOfDay;
+
+//   if (hours < 12) {
+//     timeOfDay = "morning";
+//   } else if (hours >= 12 && hours < 17) {
+//     timeOfDay = "afternoon";
+//   } else {
+//     timeOfDay = "night";
+//   }
+//   return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+// }
+
+class Greeting extends Component {
+  render() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+
+    if (hours < 12) {
+      timeOfDay = "morning";
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon";
+    } else {
+      timeOfDay = "night";
+    }
+    return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
