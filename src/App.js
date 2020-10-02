98cd315dea5db7984ae455b85f06771c4ecf95c1
esteban7590 <esteban7590@hotmail.com>
2020-10-02 10:08:50 -0500

    Intro to hooks useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
-import Form from "./form/Index";
+import { useState } from "react";
 
 const App = () => {
-  return <Form />;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
 };
 
 export default App;
